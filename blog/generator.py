a1b7d41ccf851744de0b97ff0d71f60660869141
anthonymiyoro <amiyoro@gmail.com>
2016-02-01 10:35:09 +0300

    Created metadata for my posts

--- blog/generator.py
+++ blog/generator.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template
 from werkzeug import cached_property
 import markdown
+import yaml
 
 POSTS_FILE_EXTENTION = '.md'
 app = Flask(__name__)
@@ -12,12 +13,24 @@ app = Flask(__name__)
 class Post(object):
     def __init__(self, path):
         self.path = path
+        self._initialize_metadata()
 
     @cached_property
     def html(self):
         with open(self.path, 'r') as fin:
-            content = fin.read().strip()
+            content = fin.read().split('\n\n', 1)[1].strip()
         return markdown.markdown(content)
+
+    def _initialize_metadata(self):
+        content = ''
+        with open(self.path, 'r') as fin:
+            for line in fin:
+                if not line.strip():
+                    break
+                content += line
+        self.__dict__.update(yaml.load(content))
+
+
 
 
 @app.route('/')
---
+++ blog/posts/hello.md
@@ -0,0 +1,6 @@
+title: hello, World!
+date: 2016-02-01
+
+# Hello World
+
+### This is a triple level header
