5738c81038e9aa861bbb76f79ab6d6aab82280ff
anthonymiyoro <amiyoro@gmail.com>
2016-02-02 09:42:40 +0300

    Created sorting of posts by date

--- blog/generator.py
+++ blog/generator.py
@@ -1,3 +1,4 @@
+import collections
 import os
 
 from flask import Flask, render_template, url_for, abort
@@ -8,34 +9,68 @@ import yaml
 POSTS_FILE_EXTENSION = '.md'
 
 
+class SortedDict(collections.MutableMapping):
+    def __init__(self, items=None, key=None, reverse=False):
+        self._items = {}
+        self._keys = []
+        if key:
+            self._key_fn = lambda k: key(self._items[k])
+        else:
+            self._key_fn = lambda k: self._items[k]
+        self._reverse = reverse
+
+    def __getitem__(self, key):
+        return self._items[key]
+
+    def __setitem__(self, key, value):
+        self._items[key] = value
+        if key not in self.keys:
+            self._keys.append(key)
+            self._keys.sort(key=self._key_fn, reverse=self._reverse)
+
+    def __delitem__(self, key):
+        self._items.pop(key)
+        self._keys.remove(key)
+
+    def __len__(self):
+        return len(self._keys)
+
+    def __iter__(self):
+        for key in self._keys:
+            yield key
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, self._items)
+
+
 class Blog(object):
     def __init__(self, app, root_dir='', file_ext=POSTS_FILE_EXTENSION):
         self.root_dir = root_dir
         self.file_ext = file_ext
         self._app = app
-        self._cache = {}
+        self._cache = SortedDict(key=lambda p: p.date, reverse=True)
         self._initialize_cache()
 
     @property
     def posts(self):
-            return self._cache.values()
+        return self._cache.values()
 
     def get_post_or_404(self, path):
-            # returns a 404 to the browser
-            try:
-                return self._cache[path]
-            except KeyError:
-                abort(404)
+        # returns a 404 to the browser
+        try:
+            return self._cache[path]
+        except KeyError:
+            abort(404)
 
     def _initialize_cache(self):
-           # Loops through the posts in the cache
-            for (root, dirpaths, filepaths) in os.walk(self.root_dir):
-                for filepath in filepaths:
-                    filename, ext = os.path.splitext(filepath)
-                    if ext == self.file_ext:
-                        path = os.path.join(root, filepath).replace(self.root_dir, '')
-                        post = Post(path, root_dir=self.root_dir)
-                        self._cache[post.urlpath] = post
+        # Loops through the posts in the cache
+        for (root, dirpaths, filepaths) in os.walk(self.root_dir):
+            for filepath in filepaths:
+                filename, ext = os.path.splitext(filepath)
+                if ext == self.file_ext:
+                    path = os.path.join(root, filepath).replace(self.root_dir, '')
+                    post = Post(path, root_dir=self.root_dir)
+                    self._cache[post.urlpath] = post
 
 
 # turns markdown into html
@@ -84,7 +119,3 @@ def index():
 def post(path):
     post = blog.get_post_or_404(path)
     return render_template('post.html', post=post)
-
-
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
